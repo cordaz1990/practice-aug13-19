bbf150b2614fae3f26e5fae2b491e92df20bd478
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 17:25:50 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,18 @@
-print('hello')
-print('word')
-print('hello')
+#Global Variables
+
+verbose = True
+
+def examples():
+    if verbose:
+      print('Running example')
+      
+been_called = False
+
+def example2():
+    been_called = True
+    
+been_called = False 
+
+def example2():
+    global been_called
+    been_called = True
